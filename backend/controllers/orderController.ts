98eaeddb4fef8f55751ccaabbd3234e062de0c1b
pawan <pawanpatidar137@gmail.com>
2023-10-31 17:38:50 +0530

    Create the getSingle Order, getLoggedInUser Order, getAllOrders, updateOrderStatus, deleteOrder API

--- backend/controllers/orderController.ts
+++ backend/controllers/orderController.ts
@@ -38,3 +38,147 @@ export const createNewOrder = async (req: Request, res: Response) => {
     });
   }
 };
+
+//  GET SINGLE ORDER
+
+export const getSingleOrder = async (req: Request, res: Response) => {
+  try {
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email"
+    );
+    if (!order) {
+      return res.status(409).json({
+        success: false,
+        message: "Order not found.",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Order retrive successfully.",
+      order,
+    });
+  } catch {
+    res.status(500).json({
+      success: true,
+      message: "Internal server error",
+    });
+  }
+};
+
+// GET LOGGED IN USER ORDER
+
+export const getMyOrder = async (req: Request, res: Response) => {
+  try {
+    const order = await Order.find({ user: req.user._id });
+    if (!order) {
+      return res.status(409).json({
+        success: false,
+        message: "Order not found.",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Order retrive successfully.",
+      order,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: true,
+      message: "Internal server error",
+    });
+  }
+};
+
+//  GET ALL ORDERS (--ADMIN)
+
+export const getAllOrders = async (req: Request, res: Response) => {
+  try {
+    const orders = await Order.find();
+    if (!orders) {
+      return res.status(409).json({
+        success: true,
+        message: "Order not found.",
+      });
+    }
+    let totalAmount = 0;
+    orders.forEach((items: any) => {
+      totalAmount += items.totalPrice;
+    });
+    res.status(200).json({
+      success: true,
+      message: "Orders retrive successfully.",
+      orders,
+      totalAmount,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+// UPDATE ORDER STATUS (--ADMIN)
+
+export const updateOrder = async (req: Request, res: Response) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (order.orderStatus === "Deliverd") {
+      return res.status(400).json({
+        success: true,
+        message: "You have alreay delivered this order",
+      });
+    }
+    order.orderItems.forEach(async (items: any) => {
+      await updateStock(items.product, items.quantity);
+    });
+    order.orderStatus = req.body.status;
+    if (req.body.status === "Deliverd") {
+      order.deliveredAt = Date.now();
+    }
+    await order.save({ validateBeforeSave: false });
+    res.status(200).json({
+      success: true,
+      message: "Order status pdated successfully.",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+async function updateStock(id: string, quantity: number) {
+  const product = await Product.findById(id);
+  product.stock -= quantity;
+
+  await product.save({ validateBeforeSave: false });
+}
+
+//  DELETE ORDER (--ADMIN)
+
+export const deleteOrder = async (req: Request, res: Response) => {
+  try {
+    const orderId = req.params.id;
+
+    const order = await Order.findOneAndDelete({ _id: orderId });
+    if (!order) {
+      return res.status(409).json({
+        success: false,
+        message: "Order not found.",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Order deleted successfully.",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: true,
+      message: "Internal server error.",
+    });
+  }
+};
--- backend/routes/orderRoute.ts
+++ backend/routes/orderRoute.ts
@@ -1,12 +1,33 @@
 const expresss = require("express");
 const router = expresss.Router();
-import { verifyAuthentication } from "../middleware/authMiddleware";
+import {
+  verifyAuthentication,
+  verifyRoles,
+} from "../middleware/authMiddleware";
 import { validateCreateOrderFields } from "../middleware/orderMiddleware";
 
-import { createNewOrder } from "../controllers/orderController";
+import {
+  createNewOrder,
+  deleteOrder,
+  getAllOrders,
+  getMyOrder,
+  getSingleOrder,
+  updateOrder,
+} from "../controllers/orderController";
 
 router
   .route("/createOrder")
   .post(verifyAuthentication, validateCreateOrderFields, createNewOrder);
+router.route("/getSingleOrder/:id").get(verifyAuthentication, getSingleOrder);
+router.route("/getMyOrder").get(verifyAuthentication, getMyOrder);
+router
+  .route("/getAllOrders")
+  .get(verifyAuthentication, verifyRoles("admin"), getAllOrders);
+router
+  .route("/updateOrder/:id")
+  .put(verifyAuthentication, verifyRoles("admin"), updateOrder);
+router
+  .route("/deleteOrder/:id")
+  .delete(verifyAuthentication, verifyRoles("admin"), deleteOrder);
 
 module.exports = router;
